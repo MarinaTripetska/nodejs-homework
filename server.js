83e3271cb7960501f8b930aa5581ce87cd4cb75b
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-06-15 22:31:26 +0200

    feat: add connect with db

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { PORT = 3000, DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("DB is working");
+  })
+  .then(() => {
+    app.listen(PORT);
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  })
+  .catch((error) => {
+    console.log(`ERROR: ${error.message}`);
+  });
