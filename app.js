bf23d3ddb98094d020bae9d66cf079ba1e09d9f3
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-06-26 21:38:07 +0200

    feat: add dir public, configure Express to share public files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(cors());
 app.use(express.json());
 app.use(logger(formatsLogger));
+app.use(express.static("public"));
 
 app.use("/users", authRouter);
 app.use("/api/v1/contacts", contactsRouter);
